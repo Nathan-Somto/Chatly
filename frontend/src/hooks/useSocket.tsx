665178ee70214ba1695e47f15ee61ce95523a3d0
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-07-31 12:04:04 +0100

    feat(frontend/socket): add socket.io client and hooks

---
+++ frontend/src/hooks/useSocket.tsx
@@ -0,0 +1,36 @@
+import create from 'zustand';
+import io, {Socket} from 'socket.io-client';
+
+type SocketState = {
+  socket: Socket | null;
+  isConnected: boolean;
+  connect: (url: string) => void;
+  disconnect: () => void;
+};
+
+const useSocketStore = create<SocketState>((set) => ({
+  socket: null,
+  isConnected: false,
+  connect: (url: string) => {
+    const socket = io(url);
+
+    socket.on('connect', () => {
+      set({ isConnected: true });
+    });
+
+    socket.on('disconnect', () => {
+      set({ isConnected: false });
+    });
+
+    set({ socket });
+  },
+  disconnect: () => {
+    const { socket } = useSocketStore.getState();
+    if (socket) {
+      socket.disconnect();
+      set({ socket: null, isConnected: false });
+    }
+  },
+}));
+
+export default useSocketStore;

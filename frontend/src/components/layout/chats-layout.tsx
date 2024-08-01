520c22acce46199c0eae92432f34947bf4f23779
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-08-01 16:47:39 +0100

    feat(frontend/layout): add profile fetch functionality

--- frontend/src/components/layout/chats-layout.tsx
+++ frontend/src/components/layout/chats-layout.tsx
@@ -4,12 +4,38 @@ import { cn } from "@/lib/utils";
 import { useEffect } from "react";
 import useSocketStore from "@/hooks/useSocket";
 import ConnectionStatus from "../common/ConnectionStatus";
+import { GetUserResponse } from "@/api-types";
+import { useGetQuery } from "@/hooks/query/useGetQuery";
+import { useProfileStore } from "@/hooks/useProfile";
+import { ErrorMessage } from "../common/error-message";
 
 function ChatsLayout() {
   // data fetching of user chats comes here
   const { chatId } = useParams();
   const isChatPage = typeof chatId !== "undefined";
   const { connect, disconnect } = useSocketStore();
+  const { setProfile, profile } = useProfileStore();
+  const {
+    data: response,
+    isError,
+    refetch,
+  } = useGetQuery<GetUserResponse>({
+    enabled: profile === null,
+    queryKey: ["profile"],
+    route: `/users/profile`,
+    displayToast: true,
+  });
+  if (isError) {
+    <ErrorMessage title="Failed to load user's chats" refetch={refetch} />;
+  }
+
+  useEffect(() => {
+    const data = response?.data;
+    console.log(data, response);
+    if (data) {   
+        setProfile(data?.user);
+      }
+  }, [response]);
   useEffect(() => {
     connect(import.meta.env.VITE_IO_URL);
     return () => {

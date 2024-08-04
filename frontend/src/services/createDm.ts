26c7a18f2764c6637ec6cb12f23ebd13e48f583b
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-08-04 10:03:22 +0100

    feat(frontend/services): add create Dm response type

--- frontend/src/services/createDm.ts
+++ frontend/src/services/createDm.ts
@@ -1,31 +1,52 @@
-import { CreateDmPayload } from "@/api-types";
+import { CreateDmChatResponse, CreateDmPayload } from "@/api-types";
 import { useMutate } from "@/hooks/query/useMutate";
+import { useActiveChat } from "@/hooks/useActiveChat";
+import { useMessages } from "@/hooks/useMessages";
 import { useProfileStore } from "@/hooks/useProfile";
 import { useNavigate } from "react-router-dom";
 type Props = {
-    members: CreateDmPayload["members"];
-    onComplete?: () => void;
-}
+  members: CreateDmPayload["members"];
+  onComplete?: () => void;
+};
 // uses the useMutate hook to create a dm, there is no need to emit
-export function CreateDm({members, onComplete}: Props){
-    const {profile} = useProfileStore();
-    const navigate = useNavigate()
-    const  {mutate, isPending} = useMutate({
-        defaultMessage: "Failed to create dm!",
-        method: "post",
-        route: "/chats/create-dm",
-        onSuccess(response) {
-            navigate(`/${profile?.id}/chats/${response.data?.privateChat?.id}`);
+export function CreateDm({ members, onComplete }: Props) {
+  const { profile } = useProfileStore();
+  const { setActiveChat } = useActiveChat();
+  const {setMessages} = useMessages();
+  const navigate = useNavigate();
+  const { mutate, isPending } = useMutate({
+    defaultMessage: "Failed to create dm!",
+    method: "post",
+    route: "/chats/create-dm",
+    onSuccess(response) {
+      const data: CreateDmChatResponse["privateChat"] = (
+        response.data as CreateDmChatResponse
+      )?.privateChat;
+      console.log(data)
+      const otherUser = data?.members.find(
+        (member) => member.user.username !== profile?.username
+      );
+      setActiveChat({
+        groupInfo: null,
+        dmInfo: {
+          id: data?.id,
+          avatar: otherUser?.user?.avatar ?? "",
+          username: otherUser?.user?.username ?? "",
+          lastSeen: otherUser?.user?.lastSeen,
         },
-        onSettled() {
-            onComplete && onComplete();
-        }
-    });
-   async  function handleCreate(){
-        mutate(members)
-    }
-    return {
-        handleCreate,
-        isPending
-    }
+      });
+      setMessages([]);
+      navigate(`/${profile?.id}/chats/${response.data?.privateChat?.id}`);
+    },
+    onSettled() {
+      onComplete && onComplete();
+    },
+  });
+  async function handleCreate() {
+    mutate({ members });
+  }
+  return {
+    handleCreate,
+    isPending,
+  };
 }

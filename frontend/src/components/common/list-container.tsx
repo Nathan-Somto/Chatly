d168e87efaad72c42ae35e38f55ce2beae62ec53
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-08-04 10:06:32 +0100

    feat(frontend/components/list-container): add auto animate for list update animation

--- frontend/src/components/common/list-container.tsx
+++ frontend/src/components/common/list-container.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
-export default function ListContainer({children}: {children: React.ReactNode}) {
+import React from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+export default function ListContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [parent] = useAutoAnimate({
+    easing: "ease-out",
+    duration: 350,
+  });
   return (
-    <section className="mt-8 px-3  space-y-1 lg:h-[calc(100vh-16*0.25rem)] lg:overflow-auto ">
-        {children}
+    <section
+      ref={parent}
+      className="mt-8 px-3  space-y-1 lg:h-[calc(100vh-16*0.25rem)] lg:overflow-auto "
+    >
+      {children}
     </section>
-  )
+  );
 }

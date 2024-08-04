3aa0e873a9c82723fdbcf6c149bc6668ce2c6ea9
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-08-04 15:54:17 +0100

    feat(backend/middlewares): add unauthenticated property to error handler response

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,18 +1,19 @@
 import { PrismaClientInitializationError, PrismaClientValidationError } from "@prisma/client/runtime/library";
 import { NextFunction, Request, Response } from "express";
-
 const errorHandler = (err: Error,req:Request, res: Response, next: NextFunction) => {
     let message = "something went wrong";
     console.error('[Error]: ',err.message);
+    let statusCode = err.message === 'Unauthenticated'? 400 : 500;
    if(err instanceof PrismaClientInitializationError){
     message = "could not connect to db"
    }
    if (err instanceof PrismaClientValidationError){
     message = "could not store data in db!"
    }
-   res.status(500).json({
+   res.status(statusCode).json({
     message,
-    success:false
+    success:false,
+    unauthenticated: err.message === 'Unauthenticated' ? true : false
    })
 }
 export {

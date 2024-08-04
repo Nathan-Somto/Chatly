0eaa4d072ac3c50807bb36224f31d0b1611904c7
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-08-04 15:47:18 +0100

    feat(backend/types): add message emit type

--- backend/src/types.d.ts
+++ backend/src/types.d.ts
@@ -4,3 +4,38 @@ declare global {
       interface Request extends StrictAuthProp{}
     }
   }
+type MessageEmit = {
+  message: {
+    Sender: {
+      id: string;
+      username: string;
+      avatar: string;
+      Member: {
+        role: MemberRole;
+      }[];
+    };
+    id: string;
+    body: string | null;
+    senderId: string;
+    isEditted: boolean;
+    readByIds: string[];
+    resourceUrl: string | null;
+    type: MessageType;
+    createdAt: Date;
+    chatId: string;
+    parentMessage: {
+      body: string;
+      avatar: string;
+      username: string;
+    } | null;
+    isReply: boolean;
+  };
+  chatInfo: {
+    id: string;
+    isGroup: boolean;
+    name: string;
+    avatars: string[];
+    lastSeen?: Date;
+    description?: string | null;
+  }
+}

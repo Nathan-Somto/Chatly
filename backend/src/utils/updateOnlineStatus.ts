b68a3f70eba4cbb59c0822e97e0bd923b1ec2ee4
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-08-06 18:03:42 +0100

    refactor:(backend/utils) Update updateOnlineStatus function to handle errors

--- backend/src/utils/updateOnlineStatus.ts
+++ backend/src/utils/updateOnlineStatus.ts
@@ -1,12 +1,18 @@
 import { prisma } from "../config/connectDb";
 
 export async function updateOnlineStatus(userId: string){
-    await prisma.user.update({
-        where: {
-            id: userId
-        },
-        data: {
-            lastSeen: new Date()
-        }
-    })
+    try{
+ await prisma.user.update({
+     where: {
+         id: userId
+     },
+     data: {
+         lastSeen: new Date()
+     }
+ })
+}
+catch(err){
+    console.error('[ErrorLog]:', err);
+}
+    
 }

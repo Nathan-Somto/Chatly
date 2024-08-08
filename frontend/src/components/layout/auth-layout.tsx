c5d0386c68c6244857550f01b9c951ec7de4ad41
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-08-08 09:58:03 +0100

    refactor(frontend/layout/auth-layout): Remove unnecessary code for mobile navigation

--- frontend/src/components/layout/auth-layout.tsx
+++ frontend/src/components/layout/auth-layout.tsx
@@ -1,19 +1,9 @@
-import {  Outlet, useNavigate } from "react-router-dom";
+import {  Outlet } from "react-router-dom";
 import AuthHeader from "../auth/auth-header";
-import { useMediaQuery } from "@/hooks/useMediaQuery";
-import { useEffect } from "react";
-
 
 export default function AuthLayout() {
-  const isMobile = useMediaQuery("(max-width: 1024px)")
-  const navigate = useNavigate();
-  useEffect(() => {
-    if(isMobile){
-      navigate('/')
-    }
-  }, [isMobile])
   return (
-    <div className="min-h-screen w-full bg-background  pb-8">
+    <div className="min-h-screen w-full bg-background pb-8">
       <AuthHeader/>
     <main className="min-h-screen flex items-center justify-center bg-background">
         <Outlet/>

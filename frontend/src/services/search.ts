67731b0a989a6036c023d5c9986c55e0fadb5ea1
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-08-12 21:10:05 +0100

    fix(frontend/src/services/search.ts): resolve fetching usersOnly issue

--- frontend/src/services/search.ts
+++ frontend/src/services/search.ts
@@ -1,21 +1,22 @@
 import { SearchDataResponse } from '@/api-types';
 import { useGetQuery } from '@/hooks/query/useGetQuery'
-import React from 'react'
 type Props = {
     keyword: string;
     usersOnly: boolean;
+    enabled?: boolean;
 }
-export default function Search({keyword, usersOnly}: Props) {
-    const {data: searchData, refetch, isError, isPending} = useGetQuery<SearchDataResponse>({
-        enabled: true,
+export default function Search({keyword, usersOnly, enabled= true}: Props) {
+    const {data: searchData, refetch, isError, isPending, ...rest} = useGetQuery<SearchDataResponse>({
+        enabled,
         queryKey: ['search'],
-        route: `/search?keyword=${keyword}&usersOnly${usersOnly}`,
+        route: `/search?keyword=${keyword}&usersOnly=${usersOnly}`,
         displayToast: false
     })
     return {
         searchData,
         refetch,
         isError,
-        isPending
+        isPending,
+        ...rest
     }
 }

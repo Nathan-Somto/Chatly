2d0b1e3eab600a75a220c30177376e4cd423202d
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-08-15 16:55:44 +0100

    fix(frontend/components/chat): fix message updates from socket.io

--- frontend/src/components/chat/chat-body.tsx
+++ frontend/src/components/chat/chat-body.tsx
@@ -2,21 +2,26 @@ import { DarkWall, LightWall } from "@/assets";
 import { useTheme } from "../wrappers/theme-provider";
 import { MessageSquareIcon } from "lucide-react";
 import P from "../ui/typo/P";
-import { useMessages } from "@/hooks/useMessages";
+import { ModifiedMessage, useMessages } from "@/hooks/useMessages";
 import { v4 as uuidv4 } from "uuid";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import MediaViewModal from "../modals/media-viewer-modal";
 import useSocketStore from "@/hooks/useSocket";
-import { GetMessagesResponse, MessageEmit } from "@/api-types";
+import {
+  GetMessagesResponse,
+  MessageDeleteEmit,
+  MessageEmit,
+} from "@/api-types";
 import Message from "./message";
 import { useGetQuery } from "@/hooks/query/useGetQuery";
 import { useParams } from "react-router-dom";
-import { useProfileStore } from "@/hooks/useProfile";
 import Loader from "../ui/loader";
 import { useActiveChat } from "@/hooks/useActiveChat";
 import { useMessageOptions } from "@/hooks/useMessageOptions";
 import DeleteModal from "../modals/delete-modal";
-import { useScrollTo } from "@/hooks/useScrollTo"
+import { useScrollTo } from "@/hooks/useScrollTo";
+import { useMutate } from "@/hooks/query/useMutate";
+import { useProfileStore } from "@/hooks/useProfile";
 
 function ChatBody() {
   const [openImgModal, setOpenImgModal] = useState(false);
@@ -26,80 +31,192 @@ function ChatBody() {
   const { messages, addMessage, setMessages } = useMessages();
   const { activeChat } = useActiveChat();
   const { socket } = useSocketStore();
+  const removedMessage = useRef<ModifiedMessage | null>(null);
   const { profile } = useProfileStore();
-  const {scrollToRef} = useScrollTo({
+  const { scrollToRef } = useScrollTo({
     triggerValues: [messages],
-  })
+    scrollOnMount: true,
+  });
   function openImageModal(src: string) {
     setOpenImgModal(true);
     setImgSrc(src);
   }
-  const { data: response, isPending: isFetching } =
-    useGetQuery<GetMessagesResponse>({
-      enabled: true,
-      route: `/chats/${chatId}/messages`,
-      queryKey: ["messages", chatId],
-      refetchOnMount: true,
-    });
+  const {
+    data: response,
+    isPending: isFetching,
+    refetch,
+  } = useGetQuery<GetMessagesResponse>({
+    enabled: true,
+    route: `/chats/${chatId}/messages`,
+    queryKey: ["messages", chatId],
+  });
   const {
     toggleDeleteModal,
     messageOptions: {
-      deleteModal: { id, open },
+      deleteModal: { id, open, index },
     },
+    resetOptions,
+    toggleDisabled,
   } = useMessageOptions();
+  const { mutateAsync: deleteMessage, isPending: isDeleting } =
+    useMutate<undefined>({
+      method: "delete",
+      route: `/messages/${id ?? removedMessage.current?.id}`,
+      defaultMessage: "Failed to delete message",
+      displayToast: false,
+    });
+  function onDeleteSuccess(
+    messagesCopy: Message["message"][],
+    prevIndex: number
+  ) {
+    console.log(
+      "messages copy and prevIndex in delete error",
+      messagesCopy,
+      prevIndex
+    );
+    if (chatId === undefined || profile?.id === undefined) return;
+    // get the prev message  from the deleted
+    const prevMessage =
+      messagesCopy.length === 0 || prevIndex > messagesCopy.length - 1
+        ? null
+        : messagesCopy[prevIndex];
+    console.log("prevMessage in on delete success: ", prevMessage);
+    const deleteEmit: MessageDeleteEmit = {
+      deletedMessageId: id,
+      prevMessage: prevMessage ?? null,
+      chatId,
+      userId: profile?.id,
+    };
+    // emit the socket event to delete the message
+    socket?.emit("deleteMessage", deleteEmit);
+    // reset the message options state
+    resetOptions();
+    removedMessage.current = null;
+  }
+  function onDeleteError(
+    messagesCopy: Message["message"][],
+    prevIndex: number
+  ) {
+    console.log(
+      "messages copy and prevIndex in delete error",
+      messagesCopy,
+      prevIndex
+    );
+    if (!removedMessage.current) return;
+    setMessages(
+      messagesCopy.splice(prevIndex, 0, removedMessage.current) ?? null
+    );
+    resetOptions();
+    removedMessage.current = null;
+  }
+  // Clear messages when activeChat changes
+  /*  useEffect(() => {
+    setMessages(null);
+    if (activeChat?.dmInfo || activeChat?.groupInfo) {
+      refetch();
+    }
+  }, [chatId]); */
   // get the messages from the server
   useEffect(() => {
     const data = response?.data;
-    console.log(data);
     if (data) {
       setMessages(data.messages);
     }
-  }, [response]);
+  }, [response, chatId]);
   // attach a socket io listener for new messages
   useEffect(() => {
     if (socket) {
-      const handleMessage = (data: MessageEmit) => {
-        // update the messages state
-        // check if the message is already in state.
-        const found = messages.find((item) => item.id === data.message.id);
+      // helper function that return a boolean ifwe should add the Message
+      const includeMessage = (
+        messageId: string,
+        chatId: string,
+        checkFound = true
+      ) => {
+        let found;
+        if (checkFound) {
+          found = messages?.find((item) => item.id === messageId);
+        }
         // if the chat info of the new message does not match the active chat id don't add it to the state
-        if (
-          activeChat?.dmInfo &&
-          data.chatInfo.isGroup &&
-          activeChat?.dmInfo?.id !== data.message.chatId
-        ) {
-          return;
+        const isDmChat = activeChat?.dmInfo?.id === chatId;
+        const isGroupChat = activeChat?.groupInfo?.id === chatId;
+
+        if (!(isDmChat || isGroupChat)) {
+          return false;
         }
-        if (
-          activeChat?.groupInfo &&
-          !data.chatInfo.isGroup &&
-          activeChat?.groupInfo?.id !== data.message.chatId
-        ) {
-          return;
+        if (checkFound) {
+          if (found) {
+            return false;
+          }
         }
-        if (found) {
-          return;
-        }
+        return true;
+      };
+      const handleNewMessage = (data: MessageEmit) => {
+        console.log("handle new  message: ", data);
+        if (!includeMessage(data.message.id, data.chatInfo.id)) return;
         addMessage(data.message);
       };
-      socket?.on("newMessage", handleMessage);
+      const handleUpdateMessage = (data: MessageEmit) => {
+        console.log("update message: ", data);
+        if (!includeMessage(data.message.id, data.chatInfo.id, false)) return;
+        const foundMessage = messages?.find(
+          (item) => item.id === data.message.id
+        );
+        if (foundMessage) {
+          const updatedMessages =
+            messages?.map((item) => {
+              if (item.id === data.message.id) {
+                return data.message;
+              }
+              return item;
+            }) ?? null;
+          setMessages(updatedMessages);
+        }
+      };
+      const handleDeleteMessage = (data: MessageDeleteEmit) => {
+        if (!includeMessage(data.deletedMessageId, data.chatId, false)) return;
+        const foundMessage = messages?.find(
+          (item) => item.id === data.deletedMessageId
+        );
+        if (foundMessage) {
+          const messageCopy =
+            messages?.filter((item) => item.id !== data.deletedMessageId) ??
+            null;
+          setMessages(messageCopy);
+        }
+      };
+      socket?.on("messageDeleted", handleDeleteMessage);
+      socket?.on("messageUpdated", handleUpdateMessage);
+      socket?.on("newMessage", handleNewMessage);
       return () => {
-        socket.off("newMessage", handleMessage);
+        socket.off("newMessage", handleNewMessage);
+        socket.off("messageUpdated", handleUpdateMessage);
+        socket.off("messageDeleted", handleDeleteMessage);
       };
     }
-  }, [socket, messages, activeChat]);
+  }, [socket, chatId]);
   async function handleDelete() {
-    // remove from the store
-    console.log("delete modal: ", id);
-    const messageCopy = messages.filter((message) => message.id !== id);
-    setMessages(messageCopy);
-    // remove from the server
-    // emit the delete message event
+    toggleDisabled(true);
+    const messageCopy = messages?.slice() ?? [];
+    try {
+      // remove from the store
+      console.log("delete modal: ", id, index);
+      const message = messageCopy.length > 0 ? null : messageCopy[index];
+      messageCopy?.splice(index, 1);
+      removedMessage.current = message;
+      // update ui
+      setMessages(messageCopy);
+      // remove from the server
+      await deleteMessage();
+      onDeleteSuccess(messageCopy, index - 1);
+    } catch (err) {
+      onDeleteError(messageCopy, index - 1);
+    }
   }
   function openDeleteModal(val: boolean) {
     toggleDeleteModal({
-      id: val ? id : "",
+      id,
       open: val,
+      index,
     });
   }
   return (
@@ -113,11 +230,11 @@ function ChatBody() {
         }}
         className="mt-[75px] min-h-screen mb-16 relative"
       >
-        {isFetching ? (
+        {isFetching || messages === null ? (
           <div className="h-screen flex items-center justify-center">
             <Loader withBackground={false} size="sm" />
           </div>
-        ) : messages.length == 0 ? (
+        ) : messages?.length == 0 ? (
           <div className="h-screen grid place-items-center content-center">
             <figure className="mb-5">
               <MessageSquareIcon
@@ -130,7 +247,7 @@ function ChatBody() {
             </P>
           </div>
         ) : (
-          messages.map((item, index) => {
+          messages?.map((item, index) => {
             if (item.type === "SYSTEM") {
               return (
                 <div key={index} className="flex justify-center my-8">
@@ -144,6 +261,7 @@ function ChatBody() {
                 <Message
                   message={item}
                   sending={item?.sending}
+                  failed={item?.failed}
                   openModal={openImageModal}
                   key={uuidv4()}
                   index={index}
@@ -162,7 +280,7 @@ function ChatBody() {
         deleteFn={handleDelete}
         open={open}
         onOpenChange={openDeleteModal}
-        isPending={false}
+        isPending={isDeleting}
         title="Delete Message"
         message="Are you sure you want to delete this message?"
       />

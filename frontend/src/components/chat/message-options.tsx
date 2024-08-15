ec04139302b7cbe6ca2ac8beb637d8e7521f1a60
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-08-15 16:50:22 +0100

    refactor(frontend/components/chat):  add disabled prop to message options

--- frontend/src/components/chat/message-options.tsx
+++ frontend/src/components/chat/message-options.tsx
@@ -1,61 +1,103 @@
-import React from 'react'
-import { ContextMenu, ContextMenuItem, ContextMenuContent, ContextMenuTrigger } from '../ui/context-menu'
-import { CopyIcon, FlagIcon,  PenIcon, ReplyIcon, TrashIcon } from 'lucide-react'
+import React from "react";
+import {
+  ContextMenu,
+  ContextMenuItem,
+  ContextMenuContent,
+  ContextMenuTrigger,
+} from "../ui/context-menu";
+import {
+  CopyIcon,
+  FlagIcon,
+  PenIcon,
+  ReplyIcon,
+  TrashIcon,
+} from "lucide-react";
 
-import toast from 'react-hot-toast';
-import { useMessageOptions } from '@/hooks/useMessageOptions';
+import toast from "react-hot-toast";
+import { useMessageOptions } from "@/hooks/useMessageOptions";
 type Props = {
-    isOwn: boolean;
-    handleReply?: () => void;
-    openDeleteModal?: (id: string) => void;
-    bodyText?: string;
-    id: string;
-    replyTo: ReplyTo,
-    editMessage: EditMessage
-    children: React.ReactNode
-}
-function MessageOptions({children, isOwn, bodyText, id, replyTo, editMessage}: Props) {
-    const {onEdit, onReply, toggleDeleteModal} = useMessageOptions();
-    function handleCopy(){
-        if(!bodyText) return;
-        navigator.clipboard.writeText(bodyText);
-        toast.success("copied message!")
-    }
+  isOwn: boolean;
+  bodyText?: string;
+  id: string;
+  replyTo: ReplyTo;
+  editMessage: EditMessage;
+  children: React.ReactNode;
+  index: number;
+  disabled: boolean;
+};
+function MessageOptions({
+  children,
+  isOwn,
+  bodyText,
+  id,
+  replyTo,
+  editMessage,
+  index,
+  disabled
+}: Props) {
+  const {
+    onEdit,
+    onReply,
+    toggleDeleteModal,
+    messageOptions,
+  } = useMessageOptions();
+  function handleCopy() {
+    if (!bodyText) return;
+    navigator.clipboard.writeText(bodyText);
+    toast.success("copied message!");
+  }
   return (
     <ContextMenu>
-        <ContextMenuTrigger asChild>
-            {children}
-        </ContextMenuTrigger>
-        <ContextMenuContent  className='bg-gray-50  dark:bg-[#17191C] space-y-1  rounded-md z-[9999]  py-3 px-1.5 w-[225px]'>
-            <ContextMenuItem onClick={() => onReply(replyTo)} className="gap-2 py-2 px-4 justify-start hover:opacity-80">
-                <ReplyIcon className='!opacity-50 h-5 w-5'/>
-                Reply
+      <ContextMenuTrigger asChild disabled={messageOptions.disabled || disabled}>
+        {children}
+      </ContextMenuTrigger>
+      <ContextMenuContent className="bg-gray-50  dark:bg-[#17191C] space-y-1  rounded-md z-[9999]  py-3 px-1.5 w-[225px]">
+        <ContextMenuItem
+          onClick={() => onReply(replyTo)}
+          className="gap-2 py-2 px-4 justify-start hover:opacity-80"
+        >
+          <ReplyIcon className="!opacity-50 h-5 w-5" />
+          Reply
+        </ContextMenuItem>
+        <ContextMenuItem
+          onClick={handleCopy}
+          className="gap-2 py-2 px-4 justify-start hover:opacity-80 "
+        >
+          <CopyIcon className="!opacity-50 h-5 w-5" />
+          Copy Text
+        </ContextMenuItem>
+
+        <ContextMenuItem className="gap-2 py-2 px-4 justify-start hover:opacity-80">
+          <FlagIcon className="!opacity-50 h-5 w-5" />
+          Flag Message
+        </ContextMenuItem>
+        {isOwn && (
+          <>
+            <ContextMenuItem
+              onClick={() => onEdit(editMessage)}
+              className="gap-2 py-2 px-4 justify-start hover:opacity-80 "
+            >
+              <PenIcon className="!opacity-50 h-5 w-5" />
+              Edit Message
             </ContextMenuItem>
-            <ContextMenuItem onClick={handleCopy} className="gap-2 py-2 px-4 justify-start hover:opacity-80 ">
-                <CopyIcon className='!opacity-50 h-5 w-5'/>
-                Copy Text
+            <ContextMenuItem
+              onClick={() =>
+                toggleDeleteModal({
+                  id,
+                  open: true,
+                  index,
+                })
+              }
+              className="text-destructive hover:text-destructive gap-2 py-2 px-4 justify-start hover:opacity-80"
+            >
+              <TrashIcon className="h-5 w-5" />
+              Delete Message
             </ContextMenuItem>
-           
-            <ContextMenuItem className="gap-2 py-2 px-4 justify-start hover:opacity-80">
-                <FlagIcon className='!opacity-50 h-5 w-5'/>
-                Flag Message
-            </ContextMenuItem>
-            {isOwn && (<>
-                <ContextMenuItem onClick={() => onEdit(editMessage)} className="gap-2 py-2 px-4 justify-start hover:opacity-80 ">
-                <PenIcon className='!opacity-50 h-5 w-5'/>
-                Edit Message
-            </ContextMenuItem>
-            <ContextMenuItem onClick={() => toggleDeleteModal({
-                id,
-                open: true
-            })} className="text-destructive hover:text-destructive gap-2 py-2 px-4 justify-start hover:opacity-80">
-                <TrashIcon className='h-5 w-5'/>
-                Delete Message
-            </ContextMenuItem>
-            </>)}
-        </ContextMenuContent>
+          </>
+        )}
+      </ContextMenuContent>
     </ContextMenu>
-  )
+  );
 }
 
-export default MessageOptions
+export default MessageOptions;
--- frontend/src/components/chat/message.tsx
+++ frontend/src/components/chat/message.tsx
@@ -1,6 +1,11 @@
 import { cn } from "@/lib/utils";
 import P from "../ui/typo/P";
-import { CheckCheckIcon, CheckIcon, MoreHorizontalIcon } from "lucide-react";
+import {
+  AlertCircleIcon,
+  CheckCheckIcon,
+  CheckIcon,
+  MoreHorizontalIcon,
+} from "lucide-react";
 import MessageOptions from "./message-options";
 
 type Props = Message & {
@@ -15,7 +20,7 @@ import ProfileBox from "./profile-box";
 
 function Message({
   message: {
-    Sender: { avatar, username,id: senderId },
+    Sender: { avatar, username, id: senderId },
     body,
     createdAt,
     isEditted,
@@ -28,16 +33,18 @@ function Message({
   },
   openModal,
   sending,
+  failed,
   index,
 }: Props) {
-  const {profile}= useProfileStore();
+  const { profile } = useProfileStore();
   const isOwn = profile?.id === senderId;
   const isRead = readByIds.length > 0;
   return (
     <>
-     <MessageOptions
+      <MessageOptions
         isOwn={isOwn}
         id={id}
+        disabled={!!(sending || failed)}
         replyTo={{
           parentId: id,
           text: body ?? "",
@@ -49,137 +56,142 @@ function Message({
           id,
           index,
           text: body ?? "",
-        }}>
-      <article
-        className={cn(
-          "flex gap-3 p-4 w-full  items-center",
-          isOwn && "justify-end"
-        )}
-        id={id}
+        }}
+        index={index}
       >
-        {!isOwn && (
-          <ProfileBox
-          avatar={avatar}
-          username={username}
-          id={senderId}
-          >
-          <figure
-            className={cn(
-              "rounded-full self-end h-10 w-10 flex-shrink-0 overflow-hidden",
-              isOwn && "order-2"
-            )}
-          >
-            <img
-              src={avatar}
-              alt={username + " avatar"}
-              loading="lazy"
-              className="h-full w-full object-center object-cover"
-            />
-          </figure>
-          </ProfileBox>
-        )}
-        <div className="">
-          <div
-            className={cn(
-              "max-w-full overflow-hidden space-y-1 bg-gray-300 dark:bg-[#17191c] text-gray-800 dark:text-gray-100 rounded-bl-lg rounded-br-2xl rounded-tr-2xl rounded-tl-2xl group p-3",
-              isOwn &&
-                "rounded-bl-2xl rounded-br-lg bg-brand-p1 dark:bg-[rgb(60,116,161)] text-gray-200 dark:text-gray-50",
-              resourceUrl && ""
-            )}
-          >
-            {/* Parent Message */}
-            {isReply && (
-              <div className="flex gap-x-1.5">
-                <div
-                  className={cn(
-                    "dark:bg-gray-800 bg-gray-50 rounded-tr-lg rounded-tl-lg rounded-bl-lg rounded-br-sm py-3 px-1.5",
-                    isOwn && "bg-[rgb(84,157,216)] dark:bg-[rgb(48,94,131)]"
-                  )}
-                >
-                  <p className="font-light">{parentMessage?.body}</p>
-                </div>
-                <figure
-                  className={cn(
-                    "rounded-full self-end h-8 w-8 flex-shrink-0 overflow-hidden"
-                  )}
-                >
-                  <img
-                    src={parentMessage?.avatar}
-                    alt={"reply message avatar"}
-                    loading="lazy"
-                    className="h-full w-full object-center object-cover"
-                  />
-                </figure>
-              </div>
-            )}
-            <div>
-              {resourceUrl !== null ? (
-                <div
-                  role="button"
-                  onClick={(e) => {
-                    console.log("clicked");
-                    e.stopPropagation();
-                    e.preventDefault();
-                    openModal(resourceUrl);
-                  }}
-                  className={cn(
-                    "p-2 rounded-t-lg mb-3 h-[300px] w-full bg-gray-300 dark:bg-[#17191c]",
-                    isOwn && ""
-                  )}
-                >
-                  {type === "IMAGE" ? (
-                    <figure className="h-[295px] w-full overflow-hidden rounded-lg">
-                      <img
-                        src={resourceUrl}
-                        alt="message image"
-                        loading="lazy"
-                        className="object-cover hover:scale-110 object-center h-full hover:opacity-90 transition-all ease-in duration-200 w-full"
-                      />
-                    </figure>
-                  ) : type === "VIDEO" ? (
-                    <video src={resourceUrl} playsInline loop></video>
-                  ) : null}
-                </div>
-              ) : null}
-              {body && (
-                <div>
-                  {!isOwn && (
-                    <P className="mb-[0.3rem] text-sm font-medium max-w-fit text-gray-900 dark:text-gray-200">
-                      {username}
-                    </P>
-                  )}
-                  <P className="">
-                    <LinkifiedText text={body} isOwn={isOwn} />
-                  </P>
-                  {isEditted && <P className="opacity-80 text-xs">edited</P>}
-                  {sending ? (
-                    <P className="flex items-center gap-2 opacity-70 text-sm">
-                      <span>Sending</span>{" "}
-                      <span>
-                        <MoreHorizontalIcon className="animate-bounce" />
-                      </span>
-                    </P>
-                  ) : isOwn ? (
-                    <div className="flex justify-end items-center">
-                      {isRead ? (
-                        <CheckCheckIcon size={14} />
-                      ) : (
-                        <CheckIcon size={14} />
-                      )}
-                    </div>
-                  ) : null}
+        <article
+          className={cn(
+            "flex gap-3 p-4 w-full  items-center",
+            isOwn && "justify-end"
+          )}
+          id={id}
+        >
+          {!isOwn && (
+            <ProfileBox avatar={avatar} username={username} id={senderId}>
+              <figure
+                className={cn(
+                  "rounded-full self-end h-10 w-10 flex-shrink-0 overflow-hidden",
+                  isOwn && "order-2"
+                )}
+              >
+                <img
+                  src={avatar}
+                  alt={username + " avatar"}
+                  loading="lazy"
+                  className="h-full w-full object-center object-cover"
+                />
+              </figure>
+            </ProfileBox>
+          )}
+          <div className="">
+            <div
+              className={cn(
+                "max-w-full overflow-hidden space-y-1 bg-gray-300 dark:bg-[#17191c] text-gray-800 dark:text-gray-100 rounded-bl-lg rounded-br-2xl rounded-tr-2xl rounded-tl-2xl group p-3",
+                isOwn &&
+                  "rounded-bl-2xl rounded-br-lg bg-brand-p1 dark:bg-[rgb(60,116,161)] text-gray-200 dark:text-gray-50",
+                resourceUrl && ""
+              )}
+            >
+              {/* Parent Message */}
+              {isReply && (
+                <div className="flex gap-x-1.5">
+                  <div
+                    className={cn(
+                      "dark:bg-gray-800 bg-gray-50 rounded-tr-lg rounded-tl-lg rounded-bl-lg rounded-br-sm py-3 px-1.5",
+                      isOwn && "bg-[rgb(84,157,216)] dark:bg-[rgb(48,94,131)]"
+                    )}
+                  >
+                    <p className="font-light">{parentMessage?.body}</p>
+                  </div>
+                  <figure
+                    className={cn(
+                      "rounded-full self-end h-8 w-8 flex-shrink-0 overflow-hidden"
+                    )}
+                  >
+                    <img
+                      src={parentMessage?.avatar}
+                      alt={"reply message avatar"}
+                      loading="lazy"
+                      className="h-full w-full object-center object-cover"
+                    />
+                  </figure>
                 </div>
               )}
+              <div>
+                {resourceUrl !== null ? (
+                  <div
+                    role="button"
+                    onClick={(e) => {
+                      console.log("clicked");
+                      e.stopPropagation();
+                      e.preventDefault();
+                      openModal(resourceUrl);
+                    }}
+                    className={cn(
+                      "p-2 rounded-t-lg mb-3 h-[300px] w-full bg-gray-300 dark:bg-[#17191c]",
+                      isOwn && ""
+                    )}
+                  >
+                    {type === "IMAGE" ? (
+                      <figure className="h-[295px] w-full overflow-hidden rounded-lg">
+                        <img
+                          src={resourceUrl}
+                          alt="message image"
+                          loading="lazy"
+                          className="object-cover hover:scale-110 object-center h-full hover:opacity-90 transition-all ease-in duration-200 w-full"
+                        />
+                      </figure>
+                    ) : type === "VIDEO" ? (
+                      <video src={resourceUrl} playsInline loop></video>
+                    ) : null}
+                  </div>
+                ) : null}
+                {body && (
+                  <div>
+                    {!isOwn && (
+                      <P className="mb-[0.3rem] text-sm font-medium max-w-fit text-gray-900 dark:text-gray-200">
+                        {username}
+                      </P>
+                    )}
+                    <P className="">
+                      <LinkifiedText text={body} isOwn={isOwn} />
+                    </P>
+                    {isEditted && <P className="opacity-80 text-xs">edited</P>}
+                    {sending ? (
+                      <P className="flex items-center gap-2 opacity-70 text-sm">
+                        <span>Sending</span>{" "}
+                        <span>
+                          <MoreHorizontalIcon className="animate-bounce" size={14} />
+                        </span>
+                      </P>
+                    ) : failed ? (
+                      <P className="flex items-center justify-end gap-2 dark:text-rose-300  text-red-900 text-sm">
+                        <span>Failed</span>{" "}
+                        <span>
+                          <AlertCircleIcon size={14} />
+                        </span>
+                      </P>
+                    ) : isOwn ? (
+                      <div className="flex justify-end items-center">
+                        {isRead ? (
+                          <CheckCheckIcon size={14} />
+                        ) : (
+                          <CheckIcon size={14} />
+                        )}
+                      </div>
+                    ) : null}
+                  </div>
+                )}
+              </div>
+            </div>
+            {/* Message Footer */}
+            <div className="flex items-center gap-2">
+              <P className="font-light text-sm">
+                {new Date(createdAt)?.toLocaleDateString()}
+              </P>
             </div>
           </div>
-          {/* Message Footer */}
-          <div className="flex items-center gap-2">
-            <P className="font-light text-sm">
-              {new Date(createdAt)?.toLocaleDateString()}
-            </P>
-          </div>
-        </div>
-      </article>
+        </article>
       </MessageOptions>
     </>
   );

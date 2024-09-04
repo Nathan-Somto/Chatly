0530da0ca5f8921f1ab75a7599b6acc08b8f341a
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-09-04 13:15:41 +0100

    feat(backend/services): Update backend dependencies and add livekit-server-sdk for video chat functionality

--- backend/src/router/router.ts
+++ backend/src/router/router.ts
@@ -1,5 +1,4 @@
 import { Express } from "express";
-import { upload } from "../controllers/upload.controllers";
 import searchRouter from "../routes/search.routes";
 import userRouter from "../routes/user.routes";
 import messageRouter from "../routes/message.routes";
@@ -7,16 +6,24 @@ import chatRouter from "../routes/chat.routes";
 import swaggerJSDoc from "swagger-jsdoc";
 import { swaggerOptions } from "../swagger";
 import swaggerUi from "swagger-ui-express";
+import { cloudinaryService } from "../services/cloudinary";
+import { version } from "../../package.json";
+import { livekitService } from "../services/liveKit";
+import { joinViaLink } from "../controllers/chats.controllers";
 export function router(app: Express) {
+  const apiPrefix = `api/v${version.split(".")[0]}/`;
+  console.log("apiPrefix", apiPrefix);
   const swaggerSpec = swaggerJSDoc(swaggerOptions);
   app.use(
     "/api/docs",
     swaggerUi.serve,
     swaggerUi.setup(swaggerSpec, { explorer: true })
   );
-  app.post("/api/v1/upload", upload);
-  app.use("/api/v1/search", searchRouter);
-  app.use("/api/v1/users", userRouter);
-  app.use("/api/v1/chats", chatRouter);
-  app.use("/api/v1/messages", messageRouter);
+  app.post(`/api/v1/livekit/token`, livekitService.createToken);
+  app.post(`/api/v1/join-via-link`, joinViaLink);
+  app.use(`/api/v1/chats`, chatRouter);
+  app.post(`/api/v1/upload`, cloudinaryService.upload);
+  app.use(`/api/v1/search`, searchRouter);
+  app.use(`/api/v1/users`, userRouter);
+  app.use(`/api/v1/messages`, messageRouter);
 }
